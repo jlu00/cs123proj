78e6250506d3d82025f13e727408ecc21a981842
Lyle Kim <jun9242@uchicago.edu>
2016-05-15 18:35:57 -0500

    districtclass

---
+++ district_class.py
@@ -0,0 +1,48 @@
+import heapq
+
+class District:
+	def __init__(self, centroid, tolerance, district_id):
+		self.blocks = [centroid[0]]
+		self.id = district_id
+		self.centroid = (centroid[1], centroid[2])
+		self.population = centroid[3]
+		self.tolerance = tolerance
+
+	def add_block(self, block):
+		self.blocks.append(block[1])
+		self.population += block[2]
+
+	def return_population(self):
+		return self.population
+
+	def __lt__(self, other):
+		return self.population < other.population
+
+	def __repr__(self):
+		return self.centroid
+
+def create_districts(centroid_info, tolerance):
+	districts = []
+	i = 0
+	for c in centroid_info:
+		new_district = District(c, tolerance, i)
+		districts.append(new_district)
+		i+=1		
+
+	heapq.heapify(districts)
+	return districts 
+
+def return_low_pop(districts):
+	return districts[0]
+
+
+
+
+
+
+
+
+
+
+
+
